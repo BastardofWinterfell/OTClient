9c5b36914785ed1c3879fda7d8dba989164e6456
Jon Snow <114145208+BastardofWinterfell@users.noreply.github.com>
2024-09-01 16:00:20 -0300

    Remove terminal button

--- modules/client_terminal/terminal.lua
+++ modules/client_terminal/terminal.lua
@@ -148,8 +148,7 @@ function init()
 
     terminalWindow.onDoubleClick = popWindow
 
-    terminalButton = modules.client_topmenu.addTopRightToggleButton('terminalButton', tr('Terminal') .. ' (Ctrl + T)',
-                                                          '/images/topbuttons/terminal', toggle)
+    --terminalButton = modules.client_topmenu.addTopRightToggleButton('terminalButton', tr('Terminal') .. ' (Ctrl + T)', '/images/topbuttons/terminal', toggle)
     g_keyboard.bindKeyDown('Ctrl+T', toggle)
 
     commandHistory = g_settings.getList('terminal-history')
